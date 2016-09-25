942c40c9d5a1e360516c4af844b259c8faad5912
samuelsenior <s.m.senior@soton.ac.uk>
2016-09-25 23:34:06 +0100

    Added flair filter and choosing how many posts to display

--- redditTFTS.py
+++ redditTFTS.py
@@ -1,11 +1,15 @@
 import praw
+import argparse
 """
     ToDo:
+        -Add in documentation strings to functions
+        -Check the fetching of posts actually stops when tot_posts is found
+        rather than finding say 25 long and only displaying 10 for instance
         -Make an authors blacklist as well?
         -Flair filter
+            -extend to persistant menu
         -Make it display the top len(posts) rather than always top ten
-        -Add in way to grab a slice of posts, e.g. posts 11-20, or scroll
-        down to next top ten et
+        -Add in scrolling down to next n posts
         -Add in menu persistance option
             -e.g. called with tfts -p
             -pause after reading post and then enter takes it back to the main
@@ -23,7 +27,13 @@ Created on Wed Sep 21 20:21:18 2016
 
 
 def read_in_submissions(url='https://www.reddit.com/r/talesfromtechsupport',
-                        count=10, num_sub=10, after=''):
+                        count=10, num_sub=25, after=''):
+    if num_sub >= 1000:
+        pass
+    elif num_sub > 25:
+        num_sub = num_sub * 2
+    else:
+        num_sub = 25
     para = {}
     para['count'] = count  # Not sure if I need this, should probably check
     para['limit'] = num_sub
@@ -40,34 +50,34 @@ def load_posts(s):
     for submission in submissions:
         if blacklist(submission.title) is True:
             to_fetch = to_fetch + 1
-            i = i - 1
         elif flaircheck(submission.link_flair_text) is False:
             to_fetch = to_fetch + 1
-            i = i - 1
         else:
             posts[i] = (submission.id, submission.title, submission.selftext,
                         submission.author, submission.link_flair_text,
                         submission.permalink)
+            i = i + 1
         last = submission.name
-        i = i + 1
+        if i > tot_posts:
+            break
     while to_fetch > 0:
-        i = 1
         submissions = read_in_submissions(num_sub=s, after=last)
         for submission in submissions:
             if blacklist(submission.title) is True:
                 last = submission.name
+            elif flaircheck(submission.link_flair_text) is False:
+                last = submission.name
             else:
-                posts[i + len(posts)] = (submission.id,
+                to_fetch = to_fetch - 1
+                last = submission.name
+                if to_fetch <= 0:
+                    break
+                posts[1 + len(posts)] = (submission.id,
                                          submission.title,
                                          submission.selftext,
                                          submission.author,
                                          submission.link_flair_text,
                                          submission.permalink)
-                to_fetch = to_fetch - 1
-                i = i + 1
-                last = submission.name
-                if to_fetch <= 0:
-                    break
     return posts
 
 
@@ -84,9 +94,13 @@ def blacklist(title):
 
 
 def flaircheck(flair):
-    """code that would return True if the submission flair type is the same
-    as that selected by the user, else returns false. If no flair selected
-    then default to returning true"""
+    flair = flair.split(' ')[0].lower()  # To fix 'Short r/all' instances etc
+    if args.flair == flair:
+        return True
+    elif isinstance(args.flair, type(None)):
+        return True
+    else:
+        return False
     return True
 
 
@@ -98,7 +112,7 @@ def title():
 
 
 def print_top_ten(posts):
-    for i in range(1, 11):
+    for i in range(1, tot_posts + 1):
         print("{}: ".format(i), end='')
         print_post(posts, s=i, title=2, author=2, flair=2)
 
@@ -142,7 +156,25 @@ def get_choice(tot_posts):
         exit
 
 
-tot_posts = 10
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--flair',
+                        choices=['short', 'Short', 'medium', 'Medium',
+                                 'long', 'Long', 'epic', 'Epic',
+                                 'best', 'Best', 'BEST'],
+                        required=False)
+    parser.add_argument('-n', '--num_posts', type=int, required=False)
+    args = parser.parse_args()
+    if args.flair is not None:
+        args.flair = args.flair.lower()
+    return args
+
+
+args = get_arguments()
+if isinstance(args.num_posts, int):
+    tot_posts = args.num_posts
+else:
+    tot_posts = 10
 title()
 posts = {}
 posts = load_posts(tot_posts)
